6b4b826b2e4eab5ee58727030a0e456b81795e78
Nicholas Avenell <nicholas@istic.net>
2023-01-26 00:02:11 +0000

    ➕ New version of the PDF viewer

--- resources/views/document.blade.php
+++ resources/views/document.blade.php
@@ -29,7 +29,7 @@
 
 @if ($adobe_client_id)
     <div id="pdf-view"></div>
-    <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+    <script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
     <script type="text/javascript">
       document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
         var adobeDCView = new AdobeDC.View({clientId: "{{ $adobe_client_id }}", divId: "pdf-view"});
