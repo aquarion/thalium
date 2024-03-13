5d2acc59c9acdccc2ab778110518ce50efba0425
Shift <shift@laravelshift.com>
2024-03-13 13:51:04 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -5,26 +5,6 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        Phattarachai\LaravelMobileDetect\AgentServiceProvider::class, // https://github.com/phattarachai/laravel-mobile-detect
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-
-        App\Providers\LibrisServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Agent' => Phattarachai\LaravelMobileDetect\Facades\Agent::class, //https://github.com/phattarachai/laravel-mobile-detect
